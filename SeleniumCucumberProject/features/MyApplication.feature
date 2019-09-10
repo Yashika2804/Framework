cc65919abc93a83319728d4ad9f7f41d08cac28a
ypaddamb <ypaddamb@YPADDAMB-IN.in.oracle.com>
2019-09-10 19:47:09 +0530

    Adding few lines

--- SeleniumCucumberProject/features/MyApplication.feature
+++ SeleniumCucumberProject/features/MyApplication.feature
@@ -1,7 +1,8 @@
 
-Feature: Test unibet smoke scenario
+Feature: Test facebook smoke scenario
 
   Scenario: Login with valid credentials
-    Given Open firefox and start application
+    Given Open browser and start application
     When I enter valid "yas@gmail.com" and valid "password"
     Then user should be able to login successfully
+    
--- SeleniumCucumberProject/src/stepDefinition/SmokeTest.java
+++ SeleniumCucumberProject/src/stepDefinition/SmokeTest.java
@@ -1,5 +1,10 @@
 package stepDefinition;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,17 +19,27 @@ public class SmokeTest {
 	WebDriver driver;
 	
 	
-	@Given("^Open firefox and start application$")
-	public void Open_firefox_and_start_application() throws Throwable {
-				
-		//System.setProperty("webdriver.gecko.driver", "C://Users//ypaddamb//Desktop//geckoDriver.exe");
-		//System.getProperty("user.dir")+"//drivers//geckodriver.exe");
-		//driver=new FirefoxDriver();
-		System.setProperty("webdriver.chrome.driver", "C://Users//ypaddamb//Desktop//chromedriver.exe");
-		driver=new ChromeDriver();
+	@Given("^Open browser and start application$")
+	public void Open_browser_and_start_application() throws Throwable {
+			
+		Properties prop = new Properties();		
+		prop.load(new FileInputStream(System.getProperty("user.dir")+"//config.properties"));
 		
+		String browserName=prop.getProperty("browser");
+		if(browserName.equalsIgnoreCase("Firefox"))
+		{
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//src//drivers//geckodriver.exe");
+			driver=new ChromeDriver();           
+		}
+		else if(browserName.equalsIgnoreCase("Chrome"))
+		{
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//src//drivers//chromedriver.exe");
+			driver=new ChromeDriver();           
+		}
+				
+		String url=prop.getProperty("url");
 		driver.manage().window().maximize();
-		driver.get("http://www.facebook.com");
+		driver.get(url);
 	}
 	
 	@When("^I enter valid \"([^\"]*)\" and valid \"([^\"]*)\"$")
@@ -37,6 +52,5 @@ public class SmokeTest {
 	@Then("^user should be able to login successfully$")
 	public void user_should_be_able_to_login_successfully() throws Throwable {
 		  driver.findElement(By.id("loginbutton")).click();
-	}
-	
+	}	
 }
